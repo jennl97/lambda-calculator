49eef859c5ec2c38b2b9637e09794903c17ee3fd
jennl97 <jennhott97@gmail.com>
2019-07-31 15:00:46 -0500

    First commit of Lambda Calc project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
+import numbers from "./data";
+import operators from "./data";
+import specials from "./data";
+
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
@@ -16,8 +24,11 @@ function App() {
   return (
     <div className="container">
       <Logo />
-      <div className="App">
+      <div className="App app-container">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Specials specials = {specials} />
+        <Numbers numbers = {numbers}/>
+        {/* <Operators operators = {operators} /> */}
       </div>
     </div>
   );
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import SpecialButton from "./SpecialButton";
 
 //Import your array data to from the provided data file
+import specials from "../../../data";
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
-
+  let [addSpecial, setSpecial] = useState(props.specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {/* JHL - had to set a key for it to map array  src = https://reactjs.org/docs/lists-and-keys.html#keys*/}
+
+       {
+         addSpecial.specials.map((spec, index) => {
+            return <SpecialButton setSpecial = {spec} key = {index} /> 
+            })}
     </div>
   );
 };
+
+export default Specials;
